7bb8129cdba1d87c137d1fb396d3fc819449eae1
Luis Garcia <luisgg@gmail.com>
2015-06-30 12:04:57 +0200

    configures sources.list to use local mirror via http

--- install.ausias-extra/usr/share/aurex/profile.d/aurex-extra-vars.sh
+++ install.ausias-extra/usr/share/aurex/profile.d/aurex-extra-vars.sh
@@ -1,2 +1,4 @@
 #UBUNTU_FORCEMIRROR="file:///datos/mirror/ubuntu/"
 #AUREX_FORCEMIRROR="file:///datos/mirror/aurex/"
+UBUNTU_FORCEMIRROR="http://mirror/mirror/ubuntu/"
+AUREX_FORCEMIRROR="http://mirror/mirror/aurex/"
